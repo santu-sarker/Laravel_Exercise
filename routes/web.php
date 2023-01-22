34c84c4148997ca02592c7b8533e2ff490f19841
santu-sarker <mdsantusarker@gmail.com>
2023-01-22 19:28:59 +0600

    route file updated

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', [UserController::class, 'index']);
 Route::post('contact/add_contact', [ContactController::class, 'add_contact']);
 Route::post('/contact/delete_contact', [ContactController::class, 'delete_contact']);
-// Route::get('/test', [ContactController::class, 'delete_contact']);
-
-// Route::post('/logout', LogoutController::class, 'logout');
-// Route::get("/login", function () {
-//     return view("pages.auth.login");
-// });
-
-// Route::get("/register", function () {
-//     return view("pages.auth.user_registration");
-// });
